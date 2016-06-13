a615517c815c70ea1786cdb663436806a07cc2fe
darksun <lujun9972@gmail.com>
2016-06-13 22:12:21 +0800

    add package header

--- github-marathon-notifier.el
+++ github-marathon-notifier.el
@@ -1,4 +1,32 @@
-(require 'url)
+;;; github-marathon-notifier.el --- Check if you have finished today's marathon task -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  DarkSun
+
+;; Author: Chunyang Xu <xuchunyang56@gmail.com>
+;; URL: https://github.com/lujun9972/github-marathon-notifier
+;; Package-Requires: ((emacs "24"))
+;; Keywords: github, mode-line
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is a global minor-mode. Turn it on everywhere with:
+;;
+;;   M-x github-marathon-notifier-mode
+
+;;; Code:(require 'url)
 
 (defgroup github-marathon-notifier nil
   "Github Marathon Notifier"
